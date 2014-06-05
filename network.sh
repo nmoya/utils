86b72464f04d31d22c0e5a52ded908a09944b275
nmoya <nikmoy@gmail.com>
2014-06-04 22:21:19 -0300

    Network connectivity check added

---
+++ network.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# http://magp.ie/2012/07/09/mac-osx-shell-script-to-automate-connecting-to-wifi-network/
+
+AIRPORT="en1"; #may be en0, use networksetup -listallhardwareports to check
+WIFI_NETWORK_NAME="MARCOS-PC_Network"
+WIFI_PASSWORD="4040756E6963616D7032303132"
+
+while true
+do 
+	networksetup -setairportpower $AIRPORT off
+	networksetup -setairportpower $AIRPORT on
+	sleep 10
+	 
+	networksetup -setairportnetwork $AIRPORT $WIFI_NETWORK_NAME $WIFI_PASSWORD
+	sleep 3
+
+	if networksetup -getairportnetwork $AIRPORT | grep -i -a $WIFI_NETWORK_NAME ;
+	then
+	    echo 'Connected!';
+		say "Back online!";
+
+	    exit 0
+	else
+		echo 'Connection failed!';
+		sleep 7
+	fi 
+done
